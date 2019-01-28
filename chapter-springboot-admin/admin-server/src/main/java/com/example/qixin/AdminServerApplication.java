90042a62d3ab2a1db5d5b64bf73e4619ffbd17a6
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2019-01-28 21:35:50 +0800

    Spring Boot 2.X 用于监控基于 Spring Boot 的应用

---
+++ chapter-springboot-admin/admin-server/src/main/java/com/example/qixin/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.qixin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+}
+
