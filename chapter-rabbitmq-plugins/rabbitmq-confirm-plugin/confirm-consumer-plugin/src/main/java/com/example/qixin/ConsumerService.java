b8f5e3a86eb0a9419f1cf043189bde596711f9c8
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2019-02-13 00:48:08 +0800

    消费者消息确认机制

---
+++ chapter-rabbitmq-plugins/rabbitmq-confirm-plugin/confirm-consumer-plugin/src/main/java/com/example/qixin/ConsumerService.java
@@ -0,0 +1,27 @@
+package com.example.qixin;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * 创  建   时  间： 2019/2/13 0:34
+ * 版           本: V1.0
+ * 作           者: qixin
+ * 版  权   所  有: 版权所有(C)2016-2026
+ */
+@Log4j2
+@Service
+public class ConsumerService {
+
+    @RabbitListener(queues = "qixin-queue-test-1")
+    public void process(Message message, Channel channel) throws IOException {
+        // 采用手动应答模式, 手动确认应答更为安全稳定
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+        log.info("receive: " + new String(message.getBody()));
+    }
+}
