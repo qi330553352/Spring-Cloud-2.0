bad2ab81cdd9edec8151297cf0317a0cfe96e7f9
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2019-03-07 22:58:14 +0800

    activeMQ初始化

---
+++ chapter-activeMQ-plugins/activemq-basics-plugin/src/main/java/com/example/qixin/ActivemqBasicsPluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.qixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//http://127.0.0.1:8161/admin/ admin/admin
+@SpringBootApplication
+public class ActivemqBasicsPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqBasicsPluginApplication.class, args);
+	}
+
+}
