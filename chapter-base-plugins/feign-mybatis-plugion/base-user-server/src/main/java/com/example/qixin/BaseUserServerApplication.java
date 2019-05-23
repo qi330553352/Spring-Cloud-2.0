91dfb5368dd188367ba920b7aed3ef54546d4c58
qixin <330553352@qq.com>
2019-05-23 22:32:03 +0800

    ant命令详解

--- chapter-base-plugins/feign-mybatis-plugion/base-user-server/src/main/java/com/example/qixin/BaseUserServerApplication.java
+++ chapter-base-plugins/feign-mybatis-plugion/base-user-server/src/main/java/com/example/qixin/BaseUserServerApplication.java
@@ -1,5 +1,8 @@
 package com.example.qixin;
 
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
